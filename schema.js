be96bc758afae0e65453ec012fc442f7e7389533
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-01 18:18:49 -0400

    modify

--- schema.js
+++ schema.js
@@ -73,6 +73,18 @@ const mutation = new GraphQLObjectType({
             resolve(parentValue, args) {
                 return axios.delete('http://localhost:3000/customers/' + args.id).then(res => res.data);
             }
+        },
+        modifyCustomer: {
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                name: {type: GraphQLString},
+                email: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parentValue, args) {
+                return axios.patch('http://localhost:3000/customers/' + args.id, args).then(res => res.data);
+            }
         }
     }
 });
